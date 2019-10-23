092ea94c05f0ac0daf9a650a1468961e8c8cfff6
mori <forestcrossday@gmail.com>
2019-10-23 13:30:39 +0000

    PHP/Laravel 10 再提出

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('admin/profile/edit','Admin\ProfileController@edit');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
