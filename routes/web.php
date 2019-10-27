67fae89085ae75a40a8103e8b043150abb20d0f0
mori <forestcrossday@gmail.com>
2019-10-27 13:21:54 +0000

    PHP/Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,33 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
-
 //課題３
  //
 Route::get('xxxx', 'AAAController@bbb');
 
 //課題４
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
 
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
